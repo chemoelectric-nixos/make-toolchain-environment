2c4d027662c15779959303f65529de38117b17ea
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-08-25 19:49:52 -0500

    Autotooling started. I plan to write it in Ada.

---
+++ nix-helpers/shell.nix
@@ -0,0 +1,36 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+
+with pkgs;
+mkShell {
+  buildInputs = [
+    gnat
+    gnumake
+    autoconf-archive
+    autoconf
+    autoreconfHook
+    automake117x
+    libtool
+  ];
+
+  shellHook = ''
+
+    #
+    # Sorts Mill Autoconf Archive. This may not yet be packaged for Nix.
+    #
+    # See https://bitbucket.org/sortsmill/sortsmill-autoconf-archive
+    #
+    # Here I include the autoconf macros directly from my uninstalled
+    # sources, rather than from an installed aclocal directory.
+    #
+    sortsmill_autoconf_archive=$HOME/src/sortsmill/sortsmill-autoconf-archive
+
+    export ac_cv_build=x86_64-unknown-linux-gnu
+    export ac_cv_host=x86_64-unknown-linux-gnu
+    export ac_cv_target=x86_64-unknown-linux-gnu
+    export ACLOCAL_PATH="$ACLOCAL_PATH''${ACLOCAL_PATH+:}$sortsmill_autoconf_archive:${autoconf-archive}/aclocal"
+    export PATH="${gnat}/bin:${gnumake}/bin:${autoconf}/bin:${autoreconfHook}/bin:${automake117x}/bin:${libtool}/bin:$PATH"
+  '';
+}
