041832b2b184a639293ea35b7ada46632cdfcb1d
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-08-27 09:22:22 -0500

    Reminding myself how to program in Ada. I want to save what I have so far.

--- make_toolchain_environment.adb
+++ make_toolchain_environment.adb
@@ -1,3 +1,4 @@
+--
 --  Copyright © 2025 Barry Schwartz
 --
 --  This program is free software: you can redistribute it and/or
@@ -19,16 +20,41 @@ pragma ada_2022;
 pragma wide_character_encoding (utf8);
 pragma assertion_policy (check);
 
-with posix.files;
+with ada.command_line;
+with posix, posix.files;
+with gnat.regpat;
 
 procedure make_toolchain_environment is
 
+  use posix, posix.files;
+  package cmdln renames ada.command_line;
+  package re renames gnat.regpat;
+
+  re_for_shared_library : constant re.pattern_matcher :=
+                            re.compile ("^lib.+\.so(\.[0-9]+)*$");
+
+  magic_bytes_count_for_elf : constant integer := 4;
+  magic_bytes_for_elf : constant array (0 .. 3) of integer :=
+                          ( 16#7f#, 16#45#, 16#4c#, 16#46# );
+
+  progname : string := "<program name>";
+
+  procedure dispatch (argcount : in natural;
+                      arg      : access function (number : in positive)
+                                    return string) is
+  begin
+    null;
+  end dispatch;
+
 begin
-	null;
+  cmdln.set_exit_status (cmdln.success);
+  progname := cmdln.command_name;
+	dispatch (cmdln.argument_count, cmdln.argument'access);
 end make_toolchain_environment;
 
 -- Local Variables:
 -- mode: indented-text
+-- indent-tabs-mode: nil;
 -- tab-width: 2
 -- comment-start: "-- "
 -- coding: utf-8
