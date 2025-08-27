9a62676d728253121c3e2feef7c9fe7c4dc94e64
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-08-27 09:51:36 -0500

    I finally got the gnatmake invocation correct, I think.

--- make_toolchain_environment.adb
+++ make_toolchain_environment.adb
@@ -21,12 +21,14 @@ pragma wide_character_encoding (utf8);
 pragma assertion_policy (check);
 
 with ada.command_line;
-with posix, posix.files;
+with ada.text_io;
+with ada.wide_wide_text_io;
 with gnat.regpat;
+with posix;
+with posix.files;
 
 procedure make_toolchain_environment is
 
-  use posix, posix.files;
   package cmdln renames ada.command_line;
   package re renames gnat.regpat;
 
@@ -37,18 +39,32 @@ procedure make_toolchain_environment is
   magic_bytes_for_elf : constant array (0 .. 3) of natural :=
                           ( 16#7f#, 16#45#, 16#4c#, 16#46# );
 
-  progname : string := "<program name>";
+  progname : constant string := cmdln.command_name;
+
+  procedure usage_error is
+    procedure inform_about_usage is
+      use ada.wide_wide_text_io;
+      package tio renames ada.text_io;
+    begin
+      put ("Usage: ");
+      tio.put (progname);
+      put (" symlinks dir1 dir2 ... dirN environDir");
+      new_line;
+    end inform_about_usage;
+  begin
+    inform_about_usage;
+    cmdln.set_exit_status (cmdln.failure);
+  end usage_error;
 
   procedure dispatch (argcount : in natural;
                       arg      : access function (number : in positive)
                                     return string) is
   begin
-    null;
+    usage_error;
   end dispatch;
 
 begin
   cmdln.set_exit_status (cmdln.success);
-  progname := cmdln.command_name;
 	dispatch (cmdln.argument_count, cmdln.argument'access);
 end make_toolchain_environment;
 
