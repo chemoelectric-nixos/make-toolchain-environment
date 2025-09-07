7f0a4715619e52087e1aa1320c175288be398a18
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-09-07 11:01:56 -0500

    Now it will have a manpage. When I have it ready.

--- command_line.adb
+++ command_line.adb
@@ -21,11 +21,15 @@ with ada.text_io;
 with gnat.command_line;
 with gnat.strings;
 
+with dist_version;
+
 package body command_line is
 
   use ada.command_line;
   use ada.text_io;
   use gnat.command_line;
+
+  showing_version : aliased boolean;
 
   procedure prepare_command_line_configuration
             (config : in out command_line_configuration) is
@@ -51,8 +55,32 @@ package body command_line is
     define_switch (config => config,
                    switch => "-h",
                    long_switch => "--help",
-                   help => "display this help message");
+                   help => "display this help, then exit");
+    define_switch (config => config,
+                   output => showing_version'access,
+                   long_switch => "--version",
+                   help => "output version information, then exit");
   end prepare_command_line_configuration;
+
+  procedure show_version is
+  begin
+    put (dist_version.progname);
+    put (" ");
+    put (dist_version.version);
+    new_line;
+    put ("Copyright (C) 2025 Barry Schwartz");
+    new_line;
+    put ("License GPLv3+: GNU GPL version 3 or later " &
+         "<https://gnu.org/licenses/gpl.html>.");
+    new_line;
+    put ("This is free software: you are free to change and " &
+         "redistribute it.");
+    new_line;
+    put ("There is NO WARRANTY, to the extent permitted by law.");
+    new_line (2);
+    put ("Written by Barry Schwartz.");
+    new_line;
+  end show_version;
 
   procedure interpret_the_command_line is
     config : command_line_configuration;
@@ -60,6 +88,10 @@ package body command_line is
     bail_out := false;
     prepare_command_line_configuration (config);
     getopt (config);
+    if showing_version then
+       bail_out := true;
+       show_version;
+    end if;
   exception
     when exit_from_command_line =>
       bail_out := true;
